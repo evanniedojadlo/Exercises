c0dfcf504f8ad9a6fa63b474b2ef37877601406b
evanniedojadlo <evanniedojadlo@gmail.com>
2014-07-26 16:35:43 -0500

    Added an earlier exercise for better syntax understanding

---
+++ CSSclassesinJS.js
@@ -0,0 +1,15 @@
+//CSS Classes in Javascript -- not related to a project just getting familiar with the syntax
+
+$('.dog').hide();
+
+$('#dog').hide();
+
+$('.animal').hide();
+
+$('.animal').show();
+
+$('.filter-button').css('background', 'red');
+
+$('.animal').removeClass('dog');
+
+$('.dog').hide();
